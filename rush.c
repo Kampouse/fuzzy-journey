23c4977e2d62883fc48ef6c236b555a8a97d14d0
jp <jpmartel98@gmail.com>
2021-03-21 11:09:18 +0100

    addedcondition

--- rush.c
+++ rush.c
@@ -4,15 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-
 char *cat;
 int i = 0;
 int j = 0;
 int value[16];
 cat = argv[1];
+//we could change the param of the if to allow bigger value
+//with a argv param...
+//we could pass ir as a param too...
 while(*cat)
 {
-    if(i % 2 == 0)
+    if(i % 2 == 0 && *cat > '0' && *cat < '5')
     {
        value[j] = *cat - '0';
       printf("%d",value[j]);
@@ -21,6 +23,5 @@ while(*cat)
     cat++;
     i++;
 }
-
 
 }
