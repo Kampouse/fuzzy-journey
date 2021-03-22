728f4fff94b2b03bbfef5780ada5b6bcc173fd4a
Jean-philippe Martel <jemartel@c2r4p6.42quebec.com>
2021-03-22 15:47:11 -0400

    minortweakputnbrmighthaveanerror

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "unistd.h"
-#include "stdio.h"
 int	ft_atoi(char *value)
 {
 	int		base;
